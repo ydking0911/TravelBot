9788c4fe0b7b8600f1d97094387ed22b74d863c3
ydking0911 <ydking0911@gmail.com>
2025-11-10 10:49:54 +0900

    feat: initial commit
    springboot project setting

---
+++ src/main/java/com/yd/travelbot/TravelbotApplication.java
@@ -0,0 +1,13 @@
+package com.yd.travelbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yd/travelbot/TravelbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yd.travelbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
